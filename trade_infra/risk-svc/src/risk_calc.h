61309a4b9c7c209bd8cbcf647a20f8e1498c8857
Kab Seok Kang <83808276+kangkabseok2021@users.noreply.github.com>
2026-05-16 22:04:24 +0200

    chore(risk-svc): delete C++ risk_calc sources replaced by Rust

--- trade_infra/risk-svc/src/risk_calc.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-class RiskCalc {
-public:
-    static double mtm_pnl(double net_mw, double avg_fill_price, double current_lmp);
-    static double net_exposure(double net_mw, double current_lmp);
-    static bool   limit_breach(double net_exposure_mw, double position_limit_mw);
-};
